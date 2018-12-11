ece3871dedc9c2bd237c6bd1222cb9708ccbf1dd
David Liang <davidliangx27@gmail.com>
2018-12-10 21:58:12 -0800

    shrinker

---
+++ model/data/shrink.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+
+shrink_dirs = ['yelp', 'amazon']
+
+for shrink_dir in shrink_dirs:
+  new_dir = '{}_shrunken/'.format(shrink_dir)
+  if not os.path.isdir(new_dir):
+    os.makedirs(new_dir)
+
+  for i in range(2):
+    filename = '{}/sentiment.train.{}'.format(shrink_dir, i)
+    # print(filename)
+    with open(filename,'r') as f:
+      lines = f.readlines()
+      orig_len = len(lines)
+      lines = lines[:len(lines) // 10]
+      print("new vs old lengths: {} vs {}".format(len(lines), orig_len))
+    new_filename = '{}/sentiment.train.{}'.format(new_dir, i)
+    print(new_filename)
+    with open(new_filename,'w') as f:
+      for line in lines:
+        f.write(line)   
+
+
+  for split in ['test','dev']:
+    for i in range(2):
+      shutil.copyfile('{}/sentiment.{}.{}'.format(shrink_dir, split, i), '{}/sentiment.{}.{}'.format(new_dir, split, i))
